1610b08a440dca2719564701737b834251290af5
Mahesh Tiwari <maniktiwari007email@gmail.com>
2026-04-06 18:15:40 +0530

    fix useNavigate import

--- client/src/context/AppProvider.jsx
+++ client/src/context/AppProvider.jsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState, useNavigate } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from 'axios'
 import { AppContext } from './context'
